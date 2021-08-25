e603f47003b03c04fa9620faf7b1a784659ce7c0
dharmendrakumar0 <89457617+dharmendrakumar0@users.noreply.github.com>
2021-08-25 09:09:33 +0530

    Add files via upload

---
+++ excellence_test.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 25, 2021 at 05:39 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `excellence_test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_info`
+--
+
+CREATE TABLE `user_info` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `dob` varchar(50) NOT NULL,
+  `mobile_num` varchar(50) NOT NULL,
+  `designation` varchar(50) NOT NULL,
+  `gender` varchar(50) NOT NULL,
+  `hobbies` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_info`
+--
+
+INSERT INTO `user_info` (`id`, `first_name`, `last_name`, `email`, `dob`, `mobile_num`, `designation`, `gender`, `hobbies`) VALUES
+(7, 'Test', 'Test', 'dharmendraraj12@gmail.com', '01/03/1998', '0987654321', 'dsfsd', 'Female', '[\"Yoga\",\"Cooking\",\"Reading\"]'),
+(13, 'Raj', 'Kuamr', 'testing@gmail.com', '01/03/1998', '0987654321', 'manager', 'Female', '[\"Yoga\",\"Reading\",\"Photography\"]');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_info`
+--
+ALTER TABLE `user_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_info`
+--
+ALTER TABLE `user_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
